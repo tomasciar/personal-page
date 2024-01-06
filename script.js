1e227b46c4d372fe14e0e69f62dab62c3687d012
tomasciar <tomasciarlariello@gmail.com>
2024-01-05 23:19:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const canvas = document.getElementById('background');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const colors = ['#9666ff', '#140d26', '#21a649', '#1d2675'];
+const particles = [];
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 0.5;
+    this.speedX = (Math.random() * 1 - 0.5) * 0.5;
+    this.speedY = (Math.random() * 1 - 0.5) * 0.5;
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  }
+
+  update() {
+    if (this.x + this.size > canvas.width || this.x - this.size < 0) {
+      this.speedX = -this.speedX;
+    }
+    if (this.y + this.size > canvas.height || this.y - this.size < 0) {
+      this.speedY = -this.speedY;
+    }
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  for (let i = 0; i < 75; i++) {
+    particles.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(particle => {
+    particle.update();
+    particle.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
